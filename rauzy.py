62ad0ba55dfe5801c0e10d77f349f491d09e782b
Chenxi Wu <cw538@cornell.edu>
2024-06-13 13:45:02 -0400

    Add files via upload

---
+++ rauzy.py
@@ -0,0 +1,77 @@
+from fractions import Fraction
+
+#continued fraction calculation
+def contfrac(l):
+    r=Fraction(0, 1)
+    rl=[l[-i] for i in range(1, len(l)+1)]
+    for a in rl:
+        r=1/(a+r)
+    return r
+
+lst=[i*i for i in range(1, 41)]
+
+alpha=contfrac(lst)
+
+rotation=(2, [1, 0], [1-alpha, alpha])
+
+gamma=Fraction(0, 1)
+for i in range(1, 20):
+    r=contfrac(lst[:2*i+1])
+    gamma+=Fraction(2, 1)*(r.numerator-r.denominator*alpha)
+
+print(float(alpha), float(gamma))
+
+#gamma is the length of slit
+
+y=(6, [2, 3, 1, 5, 0, 4], [gamma, 1-alpha-gamma, alpha, gamma, 1-alpha-gamma, alpha])
+
+#Rauzy induction
+def induction(ifs):
+    n=ifs[0]
+    perm=ifs[1]
+    lengths=ifs[2]
+    if lengths[n-1]<lengths[perm[-1]]:
+        split_id=perm[-1]
+        newlengths=lengths[:split_id]+[lengths[split_id]-lengths[-1], lengths[-1]]+lengths[split_id+1:-1]
+        new_perm=[]
+        for i in perm:
+            if i<=split_id:
+                new_perm+=[i]
+            elif i==n-1:
+                new_perm+=[split_id+1]
+            else:
+                new_perm+=[i+1]
+        return 1, (n, new_perm, newlengths)
+    elif lengths[n-1]>lengths[perm[-1]]:
+        newlengths=lengths[:-1]+[lengths[-1]-lengths[perm[-1]]]
+        new_perm=[]
+        for i in perm[:-1]:
+            if i==n-1:
+                new_perm+=[i, perm[-1]]
+            else:
+                new_perm+=[i]
+        return 2, (n, new_perm, newlengths)
+    else:
+        return 0, ifs
+
+sequence=[]
+
+def summary(sequence):
+    cur=0
+    count=0
+    for i in sequence:
+        if i!=cur:
+            if cur!=0:
+                print(count)
+            cur=i
+            count=1
+        else:
+            count+=1
+    return 0
+
+while(True):
+    flag, y=induction(y)
+    sequence+=[flag]
+    if flag==0:
+        break
+summary(sequence)
